77bf87e7d1c2e7a20bf468811cf4d9739f2874d0
adelaide20 <adelaiderorisang@gmail.com>
2023-11-06 14:46:01 +0200

    add new employee

--- client/src/app/pages/create/create.component.ts
+++ client/src/app/pages/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
+
+  newEmpForm = new FormGroup({
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    email: new FormControl(''),
+    contactNo: new FormControl(''),
+    emp_role: new FormControl(''),
+    start_date: new FormControl(''),
+  });
 
   constructor() { }
 
